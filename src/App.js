493b35cb46d0b41f2cb620ed7f27718619034dd8
Yaroslav0102030405 <lukovetskyi@gmail.com>
2023-10-24 12:21:48 +0300

    добавление suspense и lazy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 // import logo from './logo.svg';
 // import './App.css';
-
 // import NavBar from "./HOME-PAGE/NavBar/NavBar";
+import React, {lazy, Suspense} from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Home from './HOME-PAGE/NavBar/Home/Home';
-import Catalog from './CATALOG-PAGE/Catalog';
-import MyLibrary from './MY-LIBRARY-PAGE/myLibrary'
-import NotFoundPage from './NOT-FOUND-PAGE/NotFoundPage';
+const Home = lazy(() => import('./HOME-PAGE/NavBar/Home/Home'),);
+const Catalog = lazy(() => import('./CATALOG-PAGE/Catalog'),);
+const MyLibrary = lazy(() => import('./MY-LIBRARY-PAGE/myLibrary'));
+const NotFoundPage = lazy(() => import('./NOT-FOUND-PAGE/NotFoundPage'));
+
+// import Home from './HOME-PAGE/NavBar/Home/Home';
+// import Catalog from './CATALOG-PAGE/Catalog';
+// import MyLibrary from './MY-LIBRARY-PAGE/myLibrary'
+// import NotFoundPage from './NOT-FOUND-PAGE/NotFoundPage';
 
 function App() {
+
+
   return (
     <div className="App">
       {/* <NavBar /> */}
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/catalog" element={<Catalog />} />
-        <Route path='/mylibrare' element={<MyLibrary />} />
-        <Route path='*' element={<NotFoundPage />} />
-      </Routes>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/mylibrare" element={<MyLibrary />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
