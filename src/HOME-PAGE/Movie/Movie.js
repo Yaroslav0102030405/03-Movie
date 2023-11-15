03d210fe35bff4e54638931f8036a1abc0e1221a
Yaroslav0102030405 <lukovetskyi@gmail.com>
2023-11-15 16:31:17 +0200

    1

--- src/HOME-PAGE/Movie/Movie.js
+++ src/HOME-PAGE/Movie/Movie.js
@@ -1,22 +1,28 @@
 import React, { useState, useEffect } from "react";
 import MovieBox from "../MovieBox/MovieBox";
 import './Movie.css';
+import axios from "axios";
 // import server from "../../API-KEY/Server";
 
 const API_URL =
   'https://api.themoviedb.org/3/movie/popular?api_key=dca6039011122855ab691a23bc74a542&language=uk&page=1';
 
 const Movie = () => {
-    const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([])
+  const [query, setQuery] = useState('react')
+  const [page, setPage] = useState(1)
+  const [pageQty, setPageQty] = useState(0)
 
     useEffect(() => {
-        fetch(API_URL)
-          .then(res => res.json())
-          .then(data => {
-            console.log(data);
-            setMovies(data.results.slice(0, 15));
-          });
-    }, [])
+      fetch(API_URL + `query=${query}&page=${page}`)
+        // axios.get(API_URL + `query=${query}&page=${page}`)
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          setMovies(data.results.slice(0, 15));
+          // setPageQty(data.total_pages)
+        });
+    }, [query, page])
 
     return (
       <>
