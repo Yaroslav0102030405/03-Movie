1da918cfdedbb89d24a5f68254a04f1b0ad04dd2
Yaroslav0102030405 <lukovetskyi@gmail.com>
2023-11-15 21:48:12 +0200

    пагинация

--- src/HOME-PAGE/Movie/Movie.js
+++ src/HOME-PAGE/Movie/Movie.js
@@ -3,9 +3,12 @@ import MovieBox from "../MovieBox/MovieBox";
 import './Movie.css';
 import axios from "axios";
 // import server from "../../API-KEY/Server";
+import { Container, Pagination, Stack } from '@mui/material'
 
 const API_URL =
-  'https://api.themoviedb.org/3/movie/popular?api_key=dca6039011122855ab691a23bc74a542&language=uk&page=1';
+  'https://api.themoviedb.org/3/movie/popular?api_key=dca6039011122855ab691a23bc74a542&language=uk';
+
+  // const BASE_URL = 'http://hn.algolia.com/api/v1/search?'
 
 const Movie = () => {
   const [movies, setMovies] = useState([])
@@ -20,7 +23,7 @@ const Movie = () => {
         .then(data => {
           console.log(data);
           setMovies(data.results.slice(0, 15));
-          // setPageQty(data.total_pages)
+          setPageQty(data.total_pages)
         });
     }, [query, page])
 
@@ -32,6 +35,30 @@ const Movie = () => {
               <MovieBox key={movieReq.id} {...movieReq} />
             ))}
           </div>
+          <Container sx={{ marginTop: 5 }}>
+            <Stack spacing={2}>
+              {!!pageQty && (
+                //      <Pagination
+                //   count={pageQty}
+                //   page={page}
+                //   color="primary"
+                //   onChange={(_, num) => setPage(num)}
+                // />
+                // <Pagination
+                //   count={pageQty}
+                //   color="primary"
+                //   page={page}
+                //   onChange={(_, num) => setPage(num)}
+                // />
+                <Pagination
+                  count={pageQty}
+                  page={page}
+                  onChange={(_, num) => setPage(num)}
+                  sx={{ marginY: 0, marginX: 'auto' }}
+                />
+              )}
+            </Stack>
+          </Container>
         </div>
       </>
     );
