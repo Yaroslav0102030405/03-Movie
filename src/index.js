ebe570ebf446b6c452fe79991aa0b86bcf48c38a
Yaroslav Lukovetskyi <lukovetskyi@gmail.com>
2024-02-16 12:28:36 +0200

    0

--- src/index.js
+++ src/index.js
@@ -6,14 +6,13 @@ import App from './App';
 
 import 'modern-normalize/modern-normalize.css';
 
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
-
